421fdce9329992fa20726f5045dafba3774ea504
Carl Peto <CPeto@becrypt.com>
2025-08-12 18:05:44 +0100

    intern strings in progmem on AVR

--- lib/IRGen/GenDecl.cpp
+++ lib/IRGen/GenDecl.cpp
@@ -6038,7 +6038,8 @@ Address IRGenFunction::createAlloca(llvm::Type *type,
 /// It should be removed when fixed. rdar://problem/22674524
 llvm::Constant *
 IRGenModule::getAddrOfGlobalString(StringRef data, CStringSectionType type,
-                                   bool willBeRelativelyAddressed) {
+                                   bool willBeRelativelyAddressed,
+                                   bool storeWithFunctionsInTextSegment) {
   if (TargetInfo.OutputObjectFormat != llvm::Triple::MachO ||
       willBeRelativelyAddressed)
     type = CStringSectionType::Default;
@@ -6087,7 +6088,8 @@ IRGenModule::getAddrOfGlobalString(StringRef data, CStringSectionType type,
   }
 
   entry = createStringConstant(data, willBeRelativelyAddressed,
-                               sectionName, name);
+                               sectionName, name,
+                                storeWithFunctionsInTextSegment);
   return entry.second;
 }
 
