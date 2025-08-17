a6cfbd0ee5d7678ce30f1907c06c4060e76b6482
Carl Peto <CPeto@becrypt.com>
2025-08-17 17:54:23 +0100

    Add @interruptHandler attribute for functions.
    - interrupt handlers are actually defined by the AVR_INTR or AVR_SIGNAL calling conventions, update the tests accordingly
    - add an end to end test to make sure the assembly is correct

--- lib/Sema/TypeCheckAttr.cpp
+++ lib/Sema/TypeCheckAttr.cpp
@@ -206,6 +206,8 @@ public:
   IGNORED_ATTR(AllowFeatureSuppression)
   IGNORED_ATTR(PreInverseGenerics)
   IGNORED_ATTR(Safe)
+  IGNORED_ATTR(Warn)
+  IGNORED_ATTR(InterruptHandler)
 #undef IGNORED_ATTR
 
   void visitABIAttr(ABIAttr *attr) {
